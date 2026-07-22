4d78d8bbcc50e89d677a3a11c0cd3ec8311021ae
vadel2003 <62300184+vadel2003@users.noreply.github.com>
2026-07-22 14:28:19 +0200

    start creating base design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Kukta - @yield('title', 'Főoldal')</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <nav>
