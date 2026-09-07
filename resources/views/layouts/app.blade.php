33aaf08757173405d48d685fb3d1d14135dfffcc
vadel2003 <62300184+vadel2003@users.noreply.github.com>
2026-09-07 15:16:52 +0200

    change some design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Kukta - @yield('title', 'Főoldal')</title>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('images/favicon.svg') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}?v={{ time() }}">
     <script src="https://unpkg.com/lucide@latest"></script>
 </head>
